db56c5561a5fa73f27425f45e212f3b470752af2
filifionka15 <54937877+filifionka15@users.noreply.github.com>
2022-01-15 19:52:57 +0300

    changes in config

--- app/models.py
+++ app/models.py
@@ -133,7 +133,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
-    confirmed = db.Column(db.Boolean, default=False)
+    #default must be False, SMTP does not work
+    confirmed = db.Column(db.Boolean, default=True)
     
     name = db.Column(db.String(64))
     location = db.Column(db.String(64))
